e735b3df8e66f49a461718d50e01dc09f8c59722
Meron Hailu <hailumeron03@gmail.com>
2025-05-30 14:30:20 +0300

    week 3 push

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -47,3 +47,26 @@ export interface DirectorInterface {
     }
     return new Director();
   }
+
+  export function isDirector(employee: Director | Teacher): employee is Director {
+    return employee instanceof Director;
+  }
+  
+
+  export function executeWork(employee: Director | Teacher): string {
+    if (isDirector(employee)) {
+      return employee.workDirectorTasks();
+    } else {
+      return employee.workTeacherTasks();
+    }
+  }
+
+  export type Subjects = 'Math' | 'History';
+
+
+export function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  }
+  return 'Teaching History';
+}
