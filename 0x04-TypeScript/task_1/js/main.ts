5f0d9ad7b2082ad67209ed2a66085542f9584177
Meron Hailu <hailumeron03@gmail.com>
2025-05-30 13:52:28 +0300

    week 3 push

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -8,3 +8,57 @@ const teacher3: Teacher = {
   
   console.log(teacher3);
   
+  const director1: Directors = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+  };
+  console.log(director1);
+
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+  }
+  
+
+  const printTeacher: printTeacherFunction = (firstName, lastName) => {
+    return `${firstName.charAt(0)}. ${lastName}`;
+  };
+  
+ console.log(printTeacher("John", "Doe")); 
+
+
+interface StudentConstructor {
+    firstName: string;
+    lastName: string;
+  }
+  
+
+  interface StudentClassInterface {
+    workOnHomework(): string;
+    displayName(): string;
+  }
+
+  class StudentClass implements StudentClassInterface {
+    private firstName: string;
+    private lastName: string;
+  
+    constructor({ firstName, lastName }: StudentConstructor) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+    }
+  
+    workOnHomework(): string {
+      return 'Currently working';
+    }
+  
+    displayName(): string {
+      return this.firstName;
+    }
+  }
+
+  const student = new StudentClass({ firstName: 'Jane', lastName: 'Doe' });
+  console.log(student.displayName());     
+  console.log(student.workOnHomework());
