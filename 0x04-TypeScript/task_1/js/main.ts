e536ce3b02aa32962df1b6d5dda27a57190a280d
Meron Hailu <hailumeron03@gmail.com>
2025-05-30 12:59:48 +0300

    week 3 push

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,10 @@
+const teacher3: Teacher = {
+    firstName: 'John',
+    fullTimeEmployee: false,
+    lastName: 'Doe',
+    location: 'London',
+    contract: false,
+  };
+  
+  console.log(teacher3);
+  
